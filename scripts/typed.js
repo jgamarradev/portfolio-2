0d4a98621e419c3cbb4b17a6782e0a7a356916d7
Juan Gamarra <juangamarra.developer@gmail.com>
2024-03-01 21:36:09 -0400

    first-commit

---
+++ scripts/typed.js
@@ -0,0 +1,12 @@
+$("#typed").typed({
+  strings: [
+    "Web Developer",
+    "Front end Developer",
+    "Back end Developer",
+    "Wordpress Expert",
+  ],
+  typeSpeed: 40,
+  backSpeed: 30,
+  smartBackspace: false,
+  loop: true,
+});
