cfff3b7f1ff835cbe6ed51b56700ad64301ce959
Juan Gamarra <juangamarra.developer@gmail.com>
2024-04-18 13:41:08 -0400

    adding-testimonials-section

--- scripts/index.js
+++ scripts/index.js
@@ -13,6 +13,15 @@
         $(".nav-menu").hide();
     });
 
+    // Testimonials carousel
+    $(".testimonial-carousel").owlCarousel({
+        autoplay: true,
+        smartSpeed: 1000,
+        items: 1,
+        dots: true,
+        loop: true,
+    });
+
 
 })(jQuery);
 
